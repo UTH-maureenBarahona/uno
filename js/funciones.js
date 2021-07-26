f997692cb1bfedccd99ad6577f52fb6c828de6fe
Maureen Barahona <maureenbarahona@gmail.com>
2021-07-25 23:34:40 -0600

    cambios del modal

--- js/funciones.js
+++ js/funciones.js
@@ -1,50 +1,63 @@
-function mostrarImagen(){
-    $(document).ready(function() {  
-        $('#boton1').click(function(){
-            $('#imagen').replaceWith('<img id="imagen" src="../img/1.jpg" alt="imagen cambiada" >');
-         });
-        $('#boton2').click(function(){
-           $('#imagen').replaceWith('<img id="imagen" src="../img/2.jpg" alt="imagen cambiada" >');
-        });
-        $('#boton3').click(function(){
-            $('#imagen').replaceWith('<img id="imagen" src="../img/3.jpg" alt="imagen cambiada" >');
-         });
-         $('#boton4').click(function(){
-            $('#imagen').replaceWith('<img id="imagen" src="../img/4.jpg" alt="imagen cambiada" >');
-         });
-
-   });
-
+function mostrarImagen() {
+  $(document).ready(function () {
+    $("#boton1").click(function () {
+      $("#imagen").replaceWith(
+        '<img id="imagen" src="../img/1.jpg" alt="imagen cambiada" >'
+      );
+    });
+    $("#boton2").click(function () {
+      $("#imagen").replaceWith(
+        '<img id="imagen" src="../img/2.jpg" alt="imagen cambiada" >'
+      );
+    });
+    $("#boton3").click(function () {
+      $("#imagen").replaceWith(
+        '<img id="imagen" src="../img/3.jpg" alt="imagen cambiada" >'
+      );
+    });
+    $("#boton4").click(function () {
+      $("#imagen").replaceWith(
+        '<img id="imagen" src="../img/4.jpg" alt="imagen cambiada" >'
+      );
+    });
+  });
 }
 
-function cambioColores(){
-    $(document).ready(function() {  
-        $('#Pamarillo').mouseenter (function(){
-            $('#colores').css("background-color","yellow");
-         });  
-         $('#Pverde').mouseenter (function(){
-            $('#colores').css("background-color","green");
-         });      
-         $('#Projo').mouseenter (function(){
-            $('#colores').css("background-color","red");
-         });     
-         $('#Pnegro').mouseenter (function(){
-            $('#colores').css("background-color","black");
-         });
-         $('#Pblanco').mouseenter (function(){
-            $('#colores').css("background-color","white");
-         });
-
-   });
-
+function cambioColores() {
+  $(document).ready(function () {
+    $("#Pamarillo").mouseenter(function () {
+      $("#colores").css("background-color", "yellow");
+    });
+    $("#Pverde").mouseenter(function () {
+      $("#colores").css("background-color", "green");
+    });
+    $("#Projo").mouseenter(function () {
+      $("#colores").css("background-color", "red");
+    });
+    $("#Pnegro").mouseenter(function () {
+      $("#colores").css("background-color", "black");
+    });
+    $("#Pblanco").mouseenter(function () {
+      $("#colores").css("background-color", "white");
+    });
+  });
 }
 
-function mostrarInfoEstudiante(){
-    $(document).ready(function() {  
-        $('#datosEstudiante').dblclick (function(){
-            $('#infoEstudiante').text('Texto de sustitución');
-         }); 
+function mostrarInfoEstudiante() {
+  $(document).ready(function () {
+    $("#datosEstudiante").dblclick(function () {
+      $("#infoEstudiante").text("Texto de sustitución");
+    });
+  });
+}
 
-   });
-
+function mostrarModal() {
+  $(document).ready(function () {
+    $("#segundoEjercicio").click(function () {
+      $("#miModal").fadeIn("slow");
+      $("#BCancel").click(function () {
+        $("#miModal").fadeOut("slow");
+      });
+    });
+  });
 }
